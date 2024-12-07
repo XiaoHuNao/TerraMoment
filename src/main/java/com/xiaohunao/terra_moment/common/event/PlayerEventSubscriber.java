1afd574944d25db89b8dcad8f9255a44957002bc
XiaoHuNao <shiwenbin2250@gmail.com>
2024-12-08 00:45:11 +0800

    清除测试代码

--- src/main/java/com/xiaohunao/terra_moment/common/event/PlayerEventSubscriber.java
+++ src/main/java/com/xiaohunao/terra_moment/common/event/PlayerEventSubscriber.java
@@ -27,27 +27,10 @@ public class PlayerEventSubscriber {
 
         if (level instanceof ServerLevel serverLevel) {
             if (player.isShiftKeyDown()){
-//                TorchGodProjectile torchGodProjectile = TMEntities.TORCH_GOD.get().create(level);
-//                if (torchGodProjectile != null) {
-//                    torchGodProjectile.setPos(player.getEyePosition());
-//                    torchGodProjectile.shootFromRotation(player,player.getXRot(), player.getYRot(),0.0F,1.0F,1.0F);
-//                    level.addFreshEntity(torchGodProjectile);
-//                }
-////
-//                BlockPos pos = event.getPos();
-//                for (int i = 0; i < 5; i++) {
-//                    for (int j = 0; j < 10; j++) {
-//                        BlockPos torchPos = pos.offset(i, 0, j); // 使用offset方法根据偏移量计算新的坐标
-////                        serverLevel.setBlock(torchPos, Blocks.TORCH.defaultBlockState(),19); // 设置火把方块
-//                        serverLevel.setBlock(torchPos, Blocks.AIR.defaultBlockState(),19); // 设置火把方块
-//                    }
-//                }
-            }
 
+            }
         } else {
             if (player.isShiftKeyDown()){
-
-//                level.addParticle(TorchGodProjectile.GLIMMER,eyePosition.x,eyePosition.y,eyePosition.z,0,0.5F,0);
             }
         }
     }
