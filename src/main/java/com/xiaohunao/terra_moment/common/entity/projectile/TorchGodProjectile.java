8995651b210474a76e046ff1f57945e65dcef75d
westernat <westernat233@163.com>
2024-12-08 21:11:05 +0800

    改用粒子风暴

--- src/main/java/com/xiaohunao/terra_moment/common/entity/projectile/TorchGodProjectile.java
+++ src/main/java/com/xiaohunao/terra_moment/common/entity/projectile/TorchGodProjectile.java
@@ -1,5 +1,6 @@
 package com.xiaohunao.terra_moment.common.entity.projectile;
 
+import com.xiaohunao.terra_moment.TerraMoment;
 import com.xiaohunao.terra_moment.common.init.TMEntities;
 import com.xiaohunao.terra_moment.common.particle.options.TorchGodParticleOptions;
 import net.minecraft.core.BlockPos;
@@ -7,13 +8,14 @@ import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.projectile.AbstractHurtingProjectile;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.Vec3;
 import org.joml.Vector3f;
-
-import java.util.Random;
+import org.mesdag.particlestorm.PSGameClient;
+import org.mesdag.particlestorm.particle.ParticleEmitter;
 
 public class TorchGodProjectile extends AbstractHurtingProjectile {
-    public static final TorchGodParticleOptions GLIMMER = new TorchGodParticleOptions(new Vector3f(255.0F / 255.0F, 128.0F / 255.0F, 16.0F / 255.0F),1.5F);
+    public static final TorchGodParticleOptions GLIMMER = new TorchGodParticleOptions(new Vector3f(255.0F / 255.0F, 128.0F / 255.0F, 16.0F / 255.0F), 1.5F);
+
+    private ParticleEmitter emitter;
 
     public TorchGodProjectile(EntityType<? extends AbstractHurtingProjectile> entityType, Level level) {
         super(entityType, level);
@@ -24,8 +26,9 @@ public class TorchGodProjectile extends AbstractHurtingProjectile {
         super(TMEntities.TORCH_GOD.get(), x, y, z, level);
         this.setNoGravity(true);
     }
+
     public TorchGodProjectile(BlockPos pos, Level level) {
-        super(TMEntities.TORCH_GOD.get(),pos.getX(),pos.getY(),pos.getZ(), level);
+        super(TMEntities.TORCH_GOD.get(), pos.getX(), pos.getY(), pos.getZ(), level);
         this.setNoGravity(true);
     }
 
@@ -52,20 +55,25 @@ public class TorchGodProjectile extends AbstractHurtingProjectile {
             return;
         }
 
-
-
-        Vec3 oldPosition = new Vec3(getX(), getY(), getZ());
-        Random random = new Random();
-
-        if (level().isClientSide()) {
-            double deltaX = getX() - oldPosition.x;
-            double deltaY = getY() - oldPosition.y;
-            double deltaZ = getZ() - oldPosition.z;
-            for (double i = 0; i < 9; i ++) {
-                double coeff = i / 9.0;
-                level().addParticle(GLIMMER, oldPosition.x + deltaX * coeff, oldPosition.y + deltaY * coeff, oldPosition.z + deltaZ * coeff, 01.1f*(random.nextFloat()-0.5f), 01.1f*(random.nextFloat()-0.5f), 01.1f*(random.nextFloat()-0.5f));
-            }
+        if (level().isClientSide && emitter == null) {
+            ParticleEmitter particleEmitter = new ParticleEmitter(level(), position(), TerraMoment.asResource("torch_god"));
+            particleEmitter.attached = this;
+            this.emitter = particleEmitter;
+            PSGameClient.LOADER.addEmitter(emitter, false);
         }
+
+//        Vec3 oldPosition = new Vec3(getX(), getY(), getZ());
+//        Random random = new Random();
+//
+//        if (level().isClientSide()) {
+//            double deltaX = getX() - oldPosition.x;
+//            double deltaY = getY() - oldPosition.y;
+//            double deltaZ = getZ() - oldPosition.z;
+//            for (double i = 0; i < 9; i ++) {
+//                double coeff = i / 9.0;
+//                level().addParticle(GLIMMER, oldPosition.x + deltaX * coeff, oldPosition.y + deltaY * coeff, oldPosition.z + deltaZ * coeff, 01.1f*(random.nextFloat()-0.5f), 01.1f*(random.nextFloat()-0.5f), 01.1f*(random.nextFloat()-0.5f));
+//            }
+//        }
     }
 
 }
