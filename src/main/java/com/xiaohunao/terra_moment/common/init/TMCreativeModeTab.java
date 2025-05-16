2edabe231968af3e039535ebd31fb1f87d4bb36b
westernat <westernat233@163.com>
2025-05-16 21:02:15 +0800

    暂时去除敌怪旗与事件系统

--- src/main/java/com/xiaohunao/terra_moment/common/init/TMCreativeModeTab.java
+++ src/main/java/com/xiaohunao/terra_moment/common/init/TMCreativeModeTab.java
@@ -2,24 +2,19 @@ package com.xiaohunao.terra_moment.common.init;
 
 import com.xiaohunao.terra_moment.TerraMoment;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
-import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
-import org.confluence.terraentity.init.TEItems;
 
 public final class TMCreativeModeTab {
     public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TerraMoment.MODID);
 
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> TAB = TABS.register("tab",
-            () -> CreativeModeTab.builder().icon(() -> TMItems.BLOOD_TEAR.get().getDefaultInstance())
-                    .title(Component.translatable("creative_tab." + TerraMoment.MODID + ".tab"))
-                    .displayItems((parameters, output) -> {
-                        TMItems.ITEMS.getEntries().forEach(item -> output.accept(item.get()));
-                    })
-                    .withTabsAfter(ResourceKey.create(Registries.CREATIVE_MODE_TAB, ResourceLocation.fromNamespaceAndPath("enemybanner", "enemybanner_tab")))
-                    .withTabsBefore(TEItems.NEO_TERRA.getId())
-                    .build());
+//    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> TAB = TABS.register("tab",
+//            () -> CreativeModeTab.builder().icon(() -> TMItems.BLOOD_TEAR.get().getDefaultInstance())
+//                    .title(Component.translatable("creative_tab." + TerraMoment.MODID + ".tab"))
+//                    .displayItems((parameters, output) -> {
+//                        TMItems.ITEMS.getEntries().forEach(item -> output.accept(item.get()));
+//                    })
+//                    .withTabsAfter(ResourceKey.create(Registries.CREATIVE_MODE_TAB, ResourceLocation.fromNamespaceAndPath("enemybanner", "enemybanner_tab")))
+//                    .withTabsBefore(TEItems.NEO_TERRA.getId())
+//                    .build());
 }
