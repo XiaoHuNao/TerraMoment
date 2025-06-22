4ea80f9a3415f25da4e439838ed2d04603f6bf1d
westernat <westernat233@163.com>
2025-06-22 21:51:43 +0800

    去除晶塔的交易

--- src/main/java/com/xiaohunao/terra_moment/common/event/PlayerEventSubscriber.java
+++ src/main/java/com/xiaohunao/terra_moment/common/event/PlayerEventSubscriber.java
@@ -10,12 +10,14 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.loading.FMLEnvironment;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.GAME, modid = TerraMoment.MODID)
 public class PlayerEventSubscriber {
     @SubscribeEvent
     public static void onPlayerInteractRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
+        if (FMLEnvironment.production) return;
         Level level = event.getLevel();
         InteractionHand hand = event.getHand();
         Player player = event.getEntity();
