1be020d76be49204c61d671362afb49b7c1ac6fb
XiaoHuNao <65500205+XiaoHuNao@users.noreply.github.com>
2025-08-31 20:12:08 +0800

    给事件物品替换or条件

--- src/main/java/com/xiaohunao/terra_moment/common/item/BloodyTearItem.java
+++ src/main/java/com/xiaohunao/terra_moment/common/item/BloodyTearItem.java
@@ -1,15 +1,22 @@
 package com.xiaohunao.terra_moment.common.item;
 
 import com.xiaohunao.heaven_destiny_moment.common.automation.AutomationContext;
+import com.xiaohunao.heaven_destiny_moment.common.context.condition.common.*;
+import com.xiaohunao.heaven_destiny_moment.common.context.condition.level.DifficultyCondition;
 import com.xiaohunao.heaven_destiny_moment.common.moment.MomentInstanceBuilder;
 import com.xiaohunao.terra_moment.common.init.TMMoments;
 import com.xiaohunao.xhn_lib.common.util.MinecraftTimeUtils;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+
+import java.util.List;
 
 public class BloodyTearItem extends EventConsumableItem{
     public BloodyTearItem() {
@@ -28,7 +35,13 @@ public class BloodyTearItem extends EventConsumableItem{
                     new AutomationContext.Builder(level)
                             .addPlayer(player)
                             .addBlockPos(player.blockPosition())
-                            .build()
+                            .build(),
+                    OrCondition.of(ModLoadedCondition.of("confluence_dimension_patch"),
+                            List.of(LocationCondition.Builder.inDimension(ResourceKey.create(Registries.DIMENSION, ResourceLocation.parse("confluence_dimension_patch:otherworld"))).build()),
+                            List.of(LocationCondition.Builder.inDimension(Level.OVERWORLD).build())
+                    ),
+                    LocationCondition.Builder.inDimension(Level.OVERWORLD).build(),
+                    InvertCondition.of(DifficultyCondition.PEACEFUL)
             );
 
             if (!player.getAbilities().instabuild) {
--- src/main/java/com/xiaohunao/terra_moment/common/item/GoblinBattleStandard.java
+++ src/main/java/com/xiaohunao/terra_moment/common/item/GoblinBattleStandard.java
@@ -1,16 +1,26 @@
 package com.xiaohunao.terra_moment.common.item;
 
 import com.xiaohunao.heaven_destiny_moment.common.automation.AutomationContext;
+import com.xiaohunao.heaven_destiny_moment.common.context.condition.common.InvertCondition;
+import com.xiaohunao.heaven_destiny_moment.common.context.condition.common.LocationCondition;
+import com.xiaohunao.heaven_destiny_moment.common.context.condition.common.ModLoadedCondition;
+import com.xiaohunao.heaven_destiny_moment.common.context.condition.common.OrCondition;
+import com.xiaohunao.heaven_destiny_moment.common.context.condition.level.DifficultyCondition;
 import com.xiaohunao.heaven_destiny_moment.common.moment.MomentInstanceBuilder;
 import com.xiaohunao.terra_moment.common.init.TMMoments;
 import com.xiaohunao.xhn_lib.common.util.MinecraftTimeUtils;
 import com.xiaohunao.xhn_lib.common.util.data.MinecraftTimeNode;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+
+import java.util.List;
 
 public class GoblinBattleStandard extends EventConsumableItem{
     public GoblinBattleStandard() {
@@ -28,7 +38,13 @@ public class GoblinBattleStandard extends EventConsumableItem{
                     new AutomationContext.Builder(level)
                             .addPlayer(player)
                             .addBlockPos(player.blockPosition())
-                            .build()
+                            .build(),
+                    OrCondition.of(ModLoadedCondition.of("confluence_dimension_patch"),
+                            List.of(LocationCondition.Builder.inDimension(ResourceKey.create(Registries.DIMENSION, ResourceLocation.parse("confluence_dimension_patch:otherworld"))).build()),
+                            List.of(LocationCondition.Builder.inDimension(Level.OVERWORLD).build())
+                    ),
+                    LocationCondition.Builder.inDimension(Level.OVERWORLD).build(),
+                    InvertCondition.of(DifficultyCondition.PEACEFUL)
             );
 
             if (!player.getAbilities().instabuild) {
