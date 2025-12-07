c4d0e9ef7d5436e8f6574d42e40c6ee6a2ef5ce9
MakerTechno <102357646+makertechno@users.noreply.github.com>
2025-12-07 16:07:32 +0800

    彻底从TerraMoment中剔除PhaseJourney，临时适配史莱姆雨难度

--- src/main/java/com/xiaohunao/terra_moment/common/init/TMScalingFunctions.java
+++ src/main/java/com/xiaohunao/terra_moment/common/init/TMScalingFunctions.java
@@ -3,10 +3,9 @@ package com.xiaohunao.terra_moment.common.init;
 import com.xiaohunao.heaven_destiny_moment.HeavenDestinyMoment;
 import com.xiaohunao.heaven_destiny_moment.common.function.MomentKillEntityConditionDifficultyScalingFunction;
 import com.xiaohunao.heaven_destiny_moment.common.init.HDMRegistries;
-import com.xiaohunao.phase_journey.common.util.PhaseUtils;
+import com.xiaohunao.terra_moment.common.api.DifficultyScalerFunction;
 import com.xiaohunao.xhn_lib.api.register.holder.FlexibleHolder;
 import com.xiaohunao.xhn_lib.api.register.register.FlexibleRegister;
-import net.minecraft.resources.ResourceLocation;
 
 
 public class TMScalingFunctions {
@@ -16,7 +15,9 @@ public class TMScalingFunctions {
     public static final FlexibleHolder<MomentKillEntityConditionDifficultyScalingFunction, ?> SLIME_RAIN =
             MOMENT_KILL_ENTITY_CONDITION_DIFFICULTY_SCALING_FUNCTION.registerStatic("slime_rain", () ->
                     (baseValue, momentInstance) -> {
-                        float multipliers = PhaseUtils.getValueBasedOnPhase(ResourceLocation.tryBuild("confluence", "goblin_army_victory"), momentInstance.getLevel(), 0.5F, 1.0F);
+                        float multipliers = DifficultyScalerFunction.SLIME_RAIN_SCALER.apply(baseValue, momentInstance.getLevel());
+
+                        /*PhaseUtils.getValueBasedOnPhase(ResourceLocation.tryBuild("confluence", "goblin_army_victory"), momentInstance.getLevel(), 0.5F, 1.0F);*/
                         return (int) (baseValue * multipliers);
                     }
             );
